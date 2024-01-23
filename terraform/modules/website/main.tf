554d93c8737df1e89aef4e86b42a8693da90e167
Yoonsoo Park <yoonsoo@duck.com>
2024-01-23 12:48:33 -0500

    svg support

--- terraform/modules/website/main.tf
+++ terraform/modules/website/main.tf
@@ -23,6 +23,7 @@ locals {
     woff = "font/woff"
     woff2 = "font/woff2"
     xml = "text/xml"
+    svg = "image/svg"
   }
   upload_directory = "${path.root}/../../../public/"
   index_page = "index.html"
